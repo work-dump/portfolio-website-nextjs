f7bbfd6c2fd9db6ef3c56c4b586dace5b7178872
work-dump <hey03.aiza3@gmail.com>
2024-01-10 20:00:31 +0200

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React from "react";
-import SectionHeading from "./SectionHeading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
+import SectionHeading from "./SectionHeading";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -33,7 +32,7 @@ export default function Skills() {
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li
-            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+            className="bg-white borderBlack rounded-xl px-5 py-3"
             key={index}
             variants={fadeInAnimationVariants}
             initial="initial"
