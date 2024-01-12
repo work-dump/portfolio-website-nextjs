5dca3f002ee24cfbe6196086d3f5e1976805df52
work-dump <hey03.aiza3@gmail.com>
2024-01-12 16:32:30 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
-import { links } from "@/lib/data";
-import Link from "next/link";
-import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/ActiveSectionContext";
+import { links } from "@/lib/data";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Header = () => {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
@@ -13,7 +12,7 @@ const Header = () => {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -45,7 +44,7 @@ const Header = () => {
 
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
