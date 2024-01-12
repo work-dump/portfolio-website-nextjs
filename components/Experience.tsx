31e6cfa085bc9bf4f30a14521fce628f1e43be39
work-dump <hey03.aiza3@gmail.com>
2024-01-12 16:32:28 +0200

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTheme } from "@/context/ThemeContext";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import React from "react";
@@ -12,6 +13,7 @@ import SectionHeading from "./SectionHeading";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
+  const { theme } = useTheme();
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -21,19 +23,24 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: "#f3f4f6",
+                background:
+                  theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
+                borderRight:
+                  theme == "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solidvrgba(255,255,255,0.5)",
               }}
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background: "white",
+                background:
+                  theme == "light" ? "white" : "rgba(255,255,255,0.15)",
                 fontSize: "1.5rem",
               }}
             >
