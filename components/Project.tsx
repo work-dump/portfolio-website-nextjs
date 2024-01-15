36f1af8c6886dab22f03dd74652275564ac652a1
work-dump <hey03.aiza3@gmail.com>
2024-01-15 14:47:41 +0200

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,11 +2,21 @@
 import { projectsData } from "@/lib/data";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import { useRef } from "react";
+import { CgWebsite } from "react-icons/cg";
+import { FaGithubSquare } from "react-icons/fa";
 
 type ProjectProps = (typeof projectsData)[number];
 
-const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
+const Project = ({
+  title,
+  description,
+  tags,
+  imageUrl,
+  githubUrl,
+  websiteUrl,
+}: ProjectProps) => {
   const ref = useRef<HTMLDivElement>(null);
 
   const { scrollYProgress } = useScroll({
@@ -28,12 +38,23 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-8 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+          <div className="flex flex-col">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <div className="flex flex-wrap gap-2">
+              <Link href={githubUrl} rel="noopener noreferrer" target="_blank">
+                <FaGithubSquare />
+              </Link>
+              <Link href={websiteUrl} rel="noopener noreferrer" target="_blank">
+                <CgWebsite />
+              </Link>
+            </div>
+          </div>
+
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 line-clamp-5">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          <ul className="flex flex-wrap mt-10 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
