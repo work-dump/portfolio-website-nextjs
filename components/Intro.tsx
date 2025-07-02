b2a1e3373a5846d6811c414c55dbf5205f2d3020
Kanoi <hey03.aiza3@gmail.com>
2025-07-02 08:57:29 -0700

    Updated About_me part

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -62,9 +62,9 @@ const Intro = () => {
       >
         <span className="font-bold">Hello, I'm Ivan.</span> I'm a{" "}
         <span className="font-bold">Versatile AI Engineer</span> with{" "}
-        <span className="font-bold">more than 7 years </span> of experience. I enjoy
-        bringing <span className="italic">ideas to life!</span>. My focus is{" "}
-        <span className="underline">ML/DL & Generative AI</span>.
+        <span className="font-bold">more than 7 years </span> of experience. Am not good at anything but
+        <span className="italic"> bringing ideas to life!</span>{" "}
+        <span className="block">My focus is ML/DL & Generative AI</span>.
       </motion.h1>
 
       <motion.div
